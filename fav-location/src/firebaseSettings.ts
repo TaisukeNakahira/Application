41251f0a885ab3b8d4243fc3eef0bd92f48ef0aa
nakahira-being <nakahira@beingcorp.co.jp>
2024-09-06 10:44:42 +0900

    Firebaseが動くことを確認

---
+++ fav-location/src/firebaseSettings.ts
@@ -0,0 +1,21 @@
+// firebaseConfig.ts
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqttwv_RSlW06Qsfi7lFurMfpu9Z-jc5A",
+  authDomain: "fav-location-7b7c2.firebaseapp.com",
+  projectId: "fav-location-7b7c2",
+  storageBucket: "fav-location-7b7c2.appspot.com",
+  messagingSenderId: "363505388783",
+  appId: "1:363505388783:web:54edb292a4b804e7ec9f1a",
+  measurementId: "G-XKH740ZPDQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { app, analytics, db };
